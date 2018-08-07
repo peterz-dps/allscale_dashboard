de49d560c8313d7ef292e5efab398b76f0fb1899
Alex Hirsch <alex@dps.uibk.ac.at>
2018-08-07 14:17:56 +0200

    Add TCP capabilities

---
+++ scripts/send_test_message.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+import struct
+
+
+MESSAGE = b'{"type": "status"}'
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("localhost", 1337))
+
+    msg = struct.pack("!Q%ds" % len(MESSAGE), len(MESSAGE), MESSAGE)
+
+    s.sendall(msg)
+    s.sendall(msg)
+    s.sendall(msg)
--- server.go
+++ server.go
@@ -1,13 +1,71 @@
 package main
 
 import (
+	"encoding/binary"
+	"io"
 	"log"
-	"math/rand"
+	"net"
 	"net/http"
-	"time"
 
+	"github.com/dustin/go-broadcast"
 	"github.com/gorilla/websocket"
 )
+
+var broadcaster = broadcast.NewBroadcaster(100)
+
+// ----------------------------------------------------------------------- Main
+
+func main() {
+	go listenAndServeTCP()
+	listenAndServeHTTP()
+}
+
+// ----------------------------------------------------------------- TCP Server
+
+func listenAndServeTCP() {
+	l, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("Listening for TCP connections")
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		go handleTCPRequest(conn)
+	}
+}
+
+func handleTCPRequest(conn net.Conn) {
+	log.Println("Accepted new TCP connection.")
+	defer conn.Close()
+	defer log.Println("Closed TCP connection.")
+
+	for {
+		var size uint64
+		err := binary.Read(conn, binary.BigEndian, &size)
+		if err != nil {
+			log.Println("read size:", err)
+			break
+		}
+
+		log.Println("required length:", size)
+
+		msg := make([]byte, size)
+		_, err = io.ReadFull(conn, msg)
+		if err != nil {
+			log.Println("read msg:", err)
+			break
+		}
+
+		log.Println("Message:", string(msg))
+		broadcaster.Submit(msg)
+	}
+}
 
 // ----------------------------------------------------------------- Web Server
 
@@ -21,41 +79,25 @@ func status(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 
-	for {
-		c.WriteJSON(randMessage())
-		time.Sleep(1 * time.Second)
+	ch := make(chan interface{})
+	broadcaster.Register(ch)
+	defer broadcaster.Unregister(ch)
+
+	for msg := range ch {
+		m, ok := msg.([]byte)
+		if !ok {
+			log.Println("invalid type")
+			continue
+		}
+
+		log.Println("sending:", m)
+		c.WriteMessage(websocket.TextMessage, m)
 	}
 }
 
-func main() {
+func listenAndServeHTTP() {
 	http.HandleFunc("/status", status)
-
 	fs := http.FileServer(http.Dir("web"))
 	http.Handle("/", fs)
-
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-}
-
-// ---------------------------------------------------------- Message Generator
-
-const numNodes = 5
-
-type message struct {
-	Type    string
-	Payload interface{}
-}
-
-type loadStatus struct {
-	Node int64
-	Load int64
-}
-
-func randMessage() message {
-	return message{
-		"LoadStatus",
-		loadStatus{
-			int64(rand.Intn(numNodes) + 1),
-			int64(rand.Intn(101)),
-		},
-	}
 }
